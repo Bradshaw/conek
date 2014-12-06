4258d116504da5d96a503f3afb40d931e4e84175
Kevin Bradshaw <kevin@naturalpad.fr>
2014-12-06 17:45:28 +0100

    FUCK NODEJITSU

--- src/conek/init.lua
+++ src/conek/init.lua
@@ -1,1 +1,25 @@
 local socket = require("socket")
+math.randomseed(os.time()) 
+
+local ip = socket.dns.toip("localhost")
+--local ip = socket.dns.toip("undefined-conek.jit.su")
+
+print(type(ip),ip)
+
+local udp = socket.udp()
+local res, err
+
+udp:settimeout(0)
+
+--[[
+res, err = udp:setpeername(ip, 1337)
+print(res, err)
+res, err = udp:send("Testing")
+print(res, err)
+
+--]]
+
+res, err = udp:sendto("This message is much longer", ip, 8080)
+print(res, err)
+
+udp:close()
--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,6 @@
 require("useful")
 function love.load(arg)
+	conek = require("conek")
 	gstate = require "gamestate"
 	game = require("game")
 	gstate.registerEvents()
